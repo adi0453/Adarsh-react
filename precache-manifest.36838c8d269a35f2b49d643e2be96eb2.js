27227a67195f6459da1be9075bea8c9f20c5e73c
adi0453 <theboss1003@hotmail.com>
2022-09-02 22:18:33 +0530

    Updates

---
+++ precache-manifest.36838c8d269a35f2b49d643e2be96eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f67701fd1bb4ad9a67c",
+    "url": "/Adarsh-react/static/css/main.2a91f083.chunk.css"
+  },
+  {
+    "revision": "8f67701fd1bb4ad9a67c",
+    "url": "/Adarsh-react/static/js/main.8f67701f.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Adarsh-react/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/Adarsh-react/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "4a9a87b7d077562bf204",
+    "url": "/Adarsh-react/static/js/runtime~main.4a9a87b7.js"
+  },
+  {
+    "revision": "4cdc4e09fb8e9419e033e5325ee1a10c",
+    "url": "/Adarsh-react/index.html"
+  }
+];
